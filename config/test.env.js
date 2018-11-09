c55bdda4e14468d1fca46b4f0f446da988523bf3
cj <cj@inorway.cn>
2018-11-09 15:18:05 +0800

    换成自己的mock接口

--- config/test.env.js
+++ config/test.env.js
@@ -4,5 +4,5 @@ const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  BASE_API: '"https://easy-mock.com/mock/5be525753f52ab2b28805f9e/chan-template"',
 })
