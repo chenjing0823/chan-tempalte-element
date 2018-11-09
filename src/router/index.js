4c1f0453ea65c376be80b17825c3ca5ae758911e
cj <cj@inorway.cn>
2018-11-09 10:45:53 +0800

    my first compelete tempalte

---
+++ src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+/* Layout */
+import Layout from '../views/layout/Layout'
+
+// import HelloWorld from '@/components/HelloWorld.vue'
+Vue.use(Router)
+/**
+ * hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+ *                                if not set alwaysShow, only more than one route under the children
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    title: 'title'               the name show in submenu and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar,
+  }
+ **/
+export const constantRouterMap = [
+  { path: '/login', component: () => import('@/views/login/index'), hidden: true },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    name: 'Dashboard',
+    hidden: true,
+    children: [{
+      path: 'dashboard',
+      component: () => import('@/views/dashboard/index')
+    }]
+  },
+  {
+    path: '/example',
+    component: Layout,
+    redirect: '/example/table',
+    name: 'example',
+    alwaysShow: true,
+    meta: { title: '示例', icon: 'form' },
+    children: [{
+      path: 'table',
+      name: 'table',
+      meta: { title: '基础表单' },
+      component: () => import('@/views/example/table')
+    }]
+  }
+]
+
+export default new Router({
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
---
+++ src/views/example/table.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <el-card class="el-card">
+    <span>my first model</span>
+  </el-card>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'table'
+}
+</script>
+
+<style scoped>
+
+</style>
