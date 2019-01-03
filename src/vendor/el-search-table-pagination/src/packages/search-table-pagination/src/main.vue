2d570828b9b6aef39c4a77b064ec4a5e6f15b9ea
cj <cj@inorway.cn>
2019-01-03 14:26:38 +0800

    多选

--- src/vendor/el-search-table-pagination/src/packages/search-table-pagination/src/main.vue
+++ src/vendor/el-search-table-pagination/src/packages/search-table-pagination/src/main.vue
@@ -61,7 +61,7 @@
 
       <slot name="prepend"/>
 
-      <el-table-column label="序号" align="center" width="50" v-if="indexShow">
+      <el-table-column label="序号" align="center" width="50" v-if="indexShow" :fixed="true">
         <template slot-scope="scope">
           {{scope.$index + 1 + pagination.pageSize * (pagination.pageIndex - 1)}}
         </template>
@@ -134,9 +134,12 @@
 
     </el-table>
 
-    <div v-if="showPagination"
-         style="margin-top: 10px;text-align: right;">
+    <div v-if="checkShow" class="checkNum">
+      已勾选&nbsp;<b class="checkNumDetail">{{checkNumber}}</b>&nbsp;记录
+    </div>
+    <div v-if="showPagination">
       <el-pagination
+        style="margin-top: 10px;text-align: right;"
         @size-change="handleSizeChange"
         @current-change="handleCurrentChange"
         :current-page="pagination.pageIndex"
@@ -150,248 +153,356 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import props from './props'
-import searchForm from '../../search/src/main.vue'
+  import Vue from 'vue'
+  import props from './props'
+  import searchForm from '../../search/src/main.vue'
 
-export default {
-  name: 'ElSearchTablePagination',
-  components: {
-    searchForm
-  },
-  props,
-  data() {
-    const _this = this
-    return {
-      Vue,
-      pagination: {
-        pageIndex: 1,
-        pageSize: (() => {
-          const { pageSizes } = _this
-          if (pageSizes.length > 0) {
-            return pageSizes[0]
-          }
-          return 20
-        })()
+  export default {
+    name: 'ElSearchTablePagination',
+    components: {
+      searchForm
+    },
+    props,
+    data() {
+      const _this = this
+      return {
+        Vue,
+        pagination: {
+          pageIndex: 1,
+          pageSize: (() => {
+            const { pageSizes } = _this
+            if (pageSizes.length > 0) {
+              return pageSizes[0]
+            }
+            return 20
+          })()
+        },
+        total: 0,
+        loading: false,
+        tableData: [],
+        checkData: [],
+        allSelected: [], // 保存已经选择的
+        hasSelected: [], // 保存当前页选择的
+        cacheLocalData: []
+      }
+    },
+    computed: {
+      newSlotScope() {
+        return Number(Vue.version.replace(/\./g, '')) >= 250
+      }
+    },
+    methods: {
+      handleSizeChange(size) {
+        this.pagination.pageSize = size
+        this.dataChangeHandler()
       },
-      total: 0,
-      loading: false,
-      tableData: [],
-      cacheLocalData: []
-    }
-  },
-  computed: {
-    newSlotScope() {
-      return Number(Vue.version.replace(/\./g, '')) >= 250
-    }
-  },
-  methods: {
-    handleSizeChange(size) {
-      this.pagination.pageSize = size
-      this.dataChangeHandler()
-    },
-    handleCurrentChange(pageIndex) {
-      this.pagination.pageIndex = pageIndex
-      this.dataChangeHandler()
-    },
-    searchHandler(resetPageIndex = true) {
-      if (resetPageIndex) {
-        this.pagination.pageIndex = 1
-      }
-      this.dataChangeHandler(arguments[0])
-    },
-    dataChangeHandler() {
-      const { type } = this
-      if (type === 'local') {
-        this.dataFilterHandler(arguments[0])
-      } else if (type === 'remote') {
-        this.fetchHandler(arguments[0])
-      }
-    },
-    dataFilter(data) {
-      const { pageIndex, pageSize } = this.pagination
-      return data.filter((v, i) => {
-        return i >= (pageIndex - 1) * pageSize && i < pageIndex * pageSize
-      })
-    },
-    dataFilterHandler(formParams) {
-      const { cacheLocalData, params } = this
-      const mergeParams = Object.assign(params, formParams)
-      const validParamKeys = Object.keys(mergeParams).filter(v => {
-        return mergeParams[v] !== undefined && mergeParams[v] !== ''
-      })
-      const searchForm = this.$refs['searchForm']
-      let paramFuzzy
-      if (searchForm) {
-        paramFuzzy = searchForm.getParamFuzzy()
-      }
+      handleCurrentChange(pageIndex) {
+        this.pagination.pageIndex = pageIndex
+        this.dataChangeHandler()
+      },
+      searchHandler(resetPageIndex = true) {
+        if (resetPageIndex) {
+          this.pagination.pageIndex = 1
+        }
+        this.dataChangeHandler(arguments[0])
+      },
+      dataChangeHandler() {
+        const { type } = this
+        if (type === 'local') {
+          this.dataFilterHandler(arguments[0])
+        } else if (type === 'remote') {
+          this.fetchHandler(arguments[0])
+        }
+      },
+      dataFilter(data) {
+        const { pageIndex, pageSize } = this.pagination
+        return data.filter((v, i) => {
+          return i >= (pageIndex - 1) * pageSize && i < pageIndex * pageSize
+        })
+      },
+      dataFilterHandler(formParams) {
+        const { cacheLocalData, params } = this
+        const mergeParams = Object.assign(params, formParams)
+        const validParamKeys = Object.keys(mergeParams).filter(v => {
+          return mergeParams[v] !== undefined && mergeParams[v] !== ''
+        })
+        const searchForm = this.$refs['searchForm']
+        let paramFuzzy
+        if (searchForm) {
+          paramFuzzy = searchForm.getParamFuzzy()
+        }
 
-      if (validParamKeys.length > 0) {
-        const validData = cacheLocalData.filter(v => {
-          const valids = []
-          validParamKeys.forEach(vv => {
-            if (typeof v[vv] === 'number') {
-              valids.push(
-                paramFuzzy && paramFuzzy[vv] ? (String(v[vv]).indexOf(String(mergeParams[vv])) !== -1)
-                  : (String(v[vv]) === String(mergeParams[vv]))
-              )
+        if (validParamKeys.length > 0) {
+          const validData = cacheLocalData.filter(v => {
+            const valids = []
+            validParamKeys.forEach(vv => {
+              if (typeof v[vv] === 'number') {
+                valids.push(
+                  paramFuzzy && paramFuzzy[vv] ? (String(v[vv]).indexOf(String(mergeParams[vv])) !== -1)
+                    : (String(v[vv]) === String(mergeParams[vv]))
+                )
+              } else {
+                valids.push(
+                  paramFuzzy && paramFuzzy[vv] ? (v[vv].indexOf(mergeParams[vv]) !== -1) : (v[vv] === mergeParams[vv])
+                )
+              }
+            })
+            return valids.every(vvv => {
+              return vvv
+            })
+          })
+
+          this.tableData = this.dataFilter(validData)
+          this.total = validData.length
+        } else {
+          this.total = cacheLocalData.length
+          this.tableData = this.dataFilter(cacheLocalData)
+        }
+      },
+      fetchHandler(formParams = {}) {
+        this.loading = true
+        const {
+          fetch, url, $axios, headers,
+          listField, pageIndexKey, pageSizeKey,
+          totalField, showPagination,
+          pagination
+        } = this
+        let { method, params } = this
+        params = JSON.parse(JSON.stringify(Object.assign(params, formParams)))
+
+        if (showPagination) {
+          params = Object.assign(params, {
+            [pageIndexKey]: pagination.pageIndex,
+            [pageSizeKey]: pagination.pageSize
+          })
+        }
+        this.$emit('get-params', params)
+        let requestObject = null
+
+        if (fetch) {
+          requestObject = fetch(params)
+        } else {
+          $axios.interceptors.request.use(
+            config => {
+              Object.keys(headers).forEach(v => {
+                config.headers[v] = headers[v]
+              })
+              return config
+            },
+            error => {
+              return Promise.reject(error)
+            }
+          )
+
+          method = method.toLowerCase()
+
+          if (method === 'get') {
+            requestObject = $axios[method](url, { params })
+          } else {
+            requestObject = $axios[method](url, params)
+          }
+        }
+
+        requestObject.then(response => {
+          let result = response
+
+          if (response && !(response instanceof Array)) {
+            if (listField && listField.indexOf('.') !== -1) {
+              listField.split('.').forEach(vv => {
+                result = result[vv]
+              })
             } else {
-              valids.push(
-                paramFuzzy && paramFuzzy[vv] ? (v[vv].indexOf(mergeParams[vv]) !== -1) : (v[vv] === mergeParams[vv])
-              )
+              result = response[listField]
+            }
+          }
+
+          if (!result || !(result instanceof Array)) {
+            this.loading = false
+            throw new Error(`The result of key:${listField} is not Array.`)
+          }
+
+          if (this.dataHandler) {
+            this.tableData = result.map(this.dataHandler)
+          } else {
+            this.tableData = result
+            /**
+             * 多选 2018.12.24
+             * */
+            this.$nextTick(() => {
+              this.allSelected = this.uniqueArr(this.allSelected, this.hasSelected)
+              this.allSelected.forEach(e => {
+                this.tableData.forEach((item, i) => {
+                  if (this.isObjectValueEqual(item, e)) {
+                    this.$refs.table.toggleRowSelection(this.tableData[i])
+                  }
+                })
+              })
+              this.$emit('selection-change', this.allSelected)
+            })
+            /**
+             * 多选 2018.12.24
+             * */
+          }
+
+          let totalValue = response
+          if (Object.prototype.toString.call(response) === '[object Array]') {
+            totalValue = response.length
+          } else if (typeof response === 'object') {
+            if (totalField && totalField.indexOf('.') !== -1) {
+              totalField.split('.').forEach(vv => {
+                totalValue = totalValue[vv]
+              })
+            } else {
+              totalValue = response[totalField]
+            }
+          } else {
+            totalValue = 0
+          }
+          this.total = totalValue
+
+          this.loading = false
+        }).catch(error => {
+          console.error('Get remote data failed. ', error)
+          this.loading = false
+        })
+      },
+      /**
+       * 多选 数组去重 2018.12.24
+       * */
+      uniqueArr(arr1, arr2) {
+        // 合并两个数组
+        arr1.push(...arr2) // 或者arr1 = [...arr1, ...arr2]
+        // 去重
+        const arr3 = Array.from(new Set(arr1)) // let arr3 = [...new Set(arr1)]
+        return arr3
+      },
+      /**
+       * 多选 数组去重 2018.12.24
+       * */
+      /**
+       * 多选 判断对象是否相等 2018.12.21
+       * */
+      isObjectValueEqual(a, b) { // 判断对象是否相等
+        const aProps = Object.getOwnPropertyNames(a)
+        const bProps = Object.getOwnPropertyNames(b) // 创建属性名称数组
+        if (aProps.length !== bProps.length) { // 如果属性数量不同 对象是相等的
+          return false
+        }
+        for (let i = 0; i < (aProps.length - 1); i++) { // 如果相同属性的值不相等 对象是不相等的
+          const propName = aProps[i]
+          if (a[propName] !== b[propName]) {
+            return false
+          }
+        }
+        return true
+      },
+      /**
+       * 多选 判断对象是否相等 2018.12.21
+       * */
+      emitEventHandler(event) {
+        /**
+         * 多选 2018.12.24start
+         * */
+        if (event === 'select') {
+          const newSelected = this.allSelected
+          for (let i = 0; i < newSelected.length; i++) {
+            for (let j = 0; j < this.tableData.length; j++) {
+              if (this.isObjectValueEqual(newSelected[i], this.tableData[j])) {
+                this.allSelected.splice(i, 1)
+              }
+            }
+          }
+          this.hasSelected = []
+          Array.from(arguments).slice(1)[0].forEach(e => {
+            this.hasSelected.push(e)
+          })
+          arguments[1] = []
+          this.allSelected.forEach(item => {
+            arguments[1].push(item)
+          })
+          this.hasSelected.forEach(item => {
+            arguments[1].push(item)
+          })
+        } else if (event === 'select-all') {
+          const newSelected = this.allSelected
+          for (let i = 0; i < newSelected.length; i++) {
+            for (let j = 0; j < this.tableData.length; j++) {
+              if (this.isObjectValueEqual(newSelected[i], this.tableData[j])) {
+                this.allSelected.splice(i, 1)
+              }
+            }
+          }
+          this.hasSelected = []
+          Array.from(arguments).slice(1)[0].forEach(e => {
+            this.hasSelected.push(e)
+          })
+          arguments[1] = []
+          this.allSelected.forEach(item => {
+            arguments[1].push(item)
+          })
+          this.hasSelected.forEach(item => {
+            arguments[1].push(item)
+          })
+        }
+        /**
+         * 多选 2018.12.24end
+         * */
+        this.$emit(event, ...Array.from(arguments).slice(1))
+      },
+      loadLocalData(data) {
+        const { autoLoad } = this
+        if (!data) {
+          this.showPagination = false
+          throw new Error(`When the type is 'local', you must set attribute 'data' and 'data' must be a array.`)
+        }
+        const cacheData = JSON.parse(JSON.stringify(data))
+        this.cacheLocalData = cacheData
+        if (autoLoad) {
+          this.tableData = this.dataFilter(cacheData)
+          this.total = cacheData.length
+        }
+      }
+    },
+    mounted() {
+      // event: expand changed to `expand-change` in Element v2.x
+      this.$refs['table'].$on('expand', (row, expanded) => this.emitEventHandler('expand', row, expanded))
+      const { type, autoLoad, data, formOptions, params } = this
+      if (type === 'remote' && autoLoad) {
+        if (formOptions) {
+          this.$refs['searchForm'].getParams((error, formParams) => {
+            if (!error) {
+              this.fetchHandler(Object.assign(formParams, params))
             }
           })
-          return valids.every(vvv => {
-            return vvv
-          })
-        })
-
-        this.tableData = this.dataFilter(validData)
-        this.total = validData.length
-      } else {
-        this.total = cacheLocalData.length
-        this.tableData = this.dataFilter(cacheLocalData)
+        } else {
+          this.fetchHandler(params)
+        }
+      } else if (type === 'local') {
+        this.loadLocalData(data)
       }
     },
-    fetchHandler(formParams = {}) {
-      this.loading = true
-      const {
-        fetch, url, $axios, headers,
-        listField, pageIndexKey, pageSizeKey,
-        totalField, showPagination,
-        pagination
-      } = this
-      let { method, params } = this
-      params = JSON.parse(JSON.stringify(Object.assign(params, formParams)))
-
-      if (showPagination) {
-        params = Object.assign(params, {
-          [pageIndexKey]: pagination.pageIndex,
-          [pageSizeKey]: pagination.pageSize
-        })
+    watch: {
+      data: function(value) {
+        this.loadLocalData(value)
       }
-      this.$emit('get-params', params)
-      let requestObject = null
-
-      if (fetch) {
-        requestObject = fetch(params)
-      } else {
-        $axios.interceptors.request.use(
-          config => {
-            Object.keys(headers).forEach(v => {
-              config.headers[v] = headers[v]
-            })
-            return config
-          },
-          error => {
-            return Promise.reject(error)
-          }
-        )
-
-        method = method.toLowerCase()
-
-        if (method === 'get') {
-          requestObject = $axios[method](url, { params })
-        } else {
-          requestObject = $axios[method](url, params)
-        }
-      }
-
-      requestObject.then(response => {
-        let result = response
-
-        if (response && !(response instanceof Array)) {
-          if (listField && listField.indexOf('.') !== -1) {
-            listField.split('.').forEach(vv => {
-              result = result[vv]
-            })
-          } else {
-            result = response[listField]
-          }
-        }
-
-        if (!result || !(result instanceof Array)) {
-          this.loading = false
-          throw new Error(`The result of key:${listField} is not Array.`)
-        }
-
-        if (this.dataHandler) {
-          this.tableData = result.map(this.dataHandler)
-        } else {
-          this.tableData = result
-        }
-
-        let totalValue = response
-        if (Object.prototype.toString.call(response) === '[object Array]') {
-          totalValue = response.length
-        } else if (typeof response === 'object') {
-          if (totalField && totalField.indexOf('.') !== -1) {
-            totalField.split('.').forEach(vv => {
-              totalValue = totalValue[vv]
-            })
-          } else {
-            totalValue = response[totalField]
-          }
-        } else {
-          totalValue = 0
-        }
-        this.total = totalValue
-
-        this.loading = false
-      }).catch(error => {
-        console.error('Get remote data failed. ', error)
-        this.loading = false
-      })
-    },
-    emitEventHandler(event) {
-      this.$emit(event, ...Array.from(arguments).slice(1))
-    },
-    loadLocalData(data) {
-      const { autoLoad } = this
-      if (!data) {
-        this.showPagination = false
-        throw new Error(`When the type is 'local', you must set attribute 'data' and 'data' must be a array.`)
-      }
-      const cacheData = JSON.parse(JSON.stringify(data))
-      this.cacheLocalData = cacheData
-      if (autoLoad) {
-        this.tableData = this.dataFilter(cacheData)
-        this.total = cacheData.length
-      }
-    }
-  },
-  mounted() {
-    // event: expand changed to `expand-change` in Element v2.x
-    this.$refs['table'].$on('expand', (row, expanded) => this.emitEventHandler('expand', row, expanded))
-    const { type, autoLoad, data, formOptions, params } = this
-    if (type === 'remote' && autoLoad) {
-      if (formOptions) {
-        this.$refs['searchForm'].getParams((error, formParams) => {
-          if (!error) {
-            this.fetchHandler(Object.assign(formParams, params))
-          }
-        })
-      } else {
-        this.fetchHandler(params)
-      }
-    } else if (type === 'local') {
-      this.loadLocalData(data)
-    }
-  },
-  watch: {
-    data: function(value) {
-      this.loadLocalData(value)
     }
   }
-}
 </script>
 
 <style>
-.el-table * {
-  font-size: 8px;
-}
-.el-table .cell {
-  font-size: 8px;
-  line-height: 14px;
-}
+  .el-table * {
+    font-size: 8px;
+  }
+  .el-table .cell {
+    font-size: 8px;
+    line-height: 14px;
+  }
+  .checkNum{
+    margin-top: 5px;
+    font-size: 16px;
+    font-family: Consolas;
+  }
+  .checkNumDetail{
+    color: #FF0000;
+  }
 </style>
